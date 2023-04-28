b5a8374ff8fa047a71403ac7e6bc0f285481d295
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 11:37:36 -0600

    Adding company constraints and tests

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,6 @@
 class Company < ApplicationRecord
   has_many :fund_managers
+
+  validates :name, presence: true, allow_blank: false
+  validates :name, uniqueness: true
 end
--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a company" do
+    company = Company.new(name: "Test company")
+
+    assert company.save
+  end
+
+  test "should not create a company without name" do
+    company = Company.new
+
+    assert_not company.save
+  end
+
+  test "should not create a company that already exists" do
+    company = Company.new(name: "Test company")
+    company.save
+
+    other_company = Company.new(name: "Test company")
+
+    assert_not other_company.save
+  end
 end
