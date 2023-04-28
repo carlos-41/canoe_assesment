a188d9f70885377d8ced48e63af2f8f79fe335e4
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 12:20:22 -0600

    Adding funds controller and views

---
+++ app/controllers/funds_controller.rb
@@ -0,0 +1,55 @@
+class FundsController < ApplicationController
+  before_action :set_fund, only: %i[ show edit update destroy ]
+
+  # GET /funds or /funds.json
+  def index
+    @funds = Fund.all
+  end
+
+  # GET /funds/new
+  def new
+    @fund = Fund.new
+  end
+
+  # GET /funds/1/edit
+  def edit
+  end
+
+  # POST /funds
+  def create
+    @fund = Fund.new(fund_params)
+
+    if @fund.save
+      redirect_to funds_url, notice: "Fund was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /funds/1
+  def update
+    if @fund.update(fund_params)
+      redirect_to funds_url, notice: "Fund was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /funds/1
+  def destroy
+    @fund.destroy
+
+    redirect_to funds_url, notice: "Fund was successfully destroyed."
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_fund
+      @fund = Fund.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def fund_params
+      params.require(:fund).permit(:name, :start_year, :aliases, :fund_manager_id)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   resources :companies, except: [:show]
   resources :fund_managers, except: [:show]
+  resources :funds, except: [:show]
 end
