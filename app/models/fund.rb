eb7823dc08dbad70ee7058e590097ccc69f75279
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 11:42:13 -0600

    Adding fund constraints and tests

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -1,3 +1,5 @@
 class Fund < ApplicationRecord
   belongs_to :fund_manager
+
+  validates :name, :start_year , presence: true, allow_blank: false
 end
--- test/models/fund_test.rb
+++ test/models/fund_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class FundTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = Company.create(name: "Test company")
+    @fund_manager = FundManager.create(company_id: @company.id, name: @company.name)
+  end
+
+  test "should create a fund" do
+    fund = Fund.new(fund_manager_id: @fund_manager.id, name: "Fund 1", start_year: "2010", aliases: "Fund One")
+    
+    assert fund.save
+  end
+
+  test "should not create a fund without fund manager" do
+    fund = Fund.new(fund_manager_id: nil)
+
+    assert_not fund.save
+  end
+
+  test "should not create a fund without start_year" do
+    fund = Fund.new(fund_manager_id: @fund_manager.id, name: "Fund 1", aliases: "Fund One")
+
+    assert_not fund.save
+  end
 end
