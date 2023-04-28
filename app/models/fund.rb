961bba8f49580d0c608749dc9d6ac5ce3961878c
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 12:54:26 -0600

    Adding find potential duplicate after fund creation

--- app/models/fund.rb
+++ app/models/fund.rb
@@ -3,4 +3,28 @@ class Fund < ApplicationRecord
 
   validates :name, :start_year , presence: true, allow_blank: false
   validates :start_year, numericality: { in: 1900..Date.today.year, message: "should be between 1900-#{Date.today.year}" }
+
+  after_create :find_potential_duplicate
+
+  #ToDo this can be extracted to a background worker for a better perfomance
+  def find_potential_duplicate
+    potential_funds = Fund.where(fund_manager_id: fund_manager_id).where.not(id: id)
+
+    #Check duplicate fund name + fund manager
+    duplicated_fund = potential_funds.find_by(name: name)
+
+    if duplicated_fund.nil?
+      #Check duplicate aliases + fund manager
+      potential_funds.each do |potential_fund|
+        if potential_fund.aliases.split(",").include?(name)
+          duplicated_fund = potential_fund
+          break
+        end
+      end
+    end
+
+    unless duplicated_fund.nil?
+      DuplicateFundWarning.create(message: "Duplicated fund", fund_id_1: duplicated_fund.id, fund_id_2: id)
+    end
+  end
 end
--- test/models/duplicate_fund_warning_test.rb
+++ test/models/duplicate_fund_warning_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 class DuplicateFundWarningTest < ActiveSupport::TestCase
+  def setup
+    @company = Company.create(name: "Test company")
+    @fund_manager = FundManager.create(company_id: @company.id, name: @company.name)
+    @fund = Fund.create(fund_manager_id: @fund_manager.id, name: "Fund 1", start_year: "2010", aliases: "Fund One")
+  end
+
   test "should create a duplicate fund warning " do
     duplicate_fund_warning = DuplicateFundWarning.new(message: "Test warning", fund_id_1: 1, fund_id_2: 2)
 
     assert duplicate_fund_warning.save
+  end
+
+  test "should create a duplicate fund warning due same fund manager and name" do
+    duplicated_fund = Fund.new(fund_manager_id: @fund_manager.id, name: "Fund 1", start_year: "2019")
+    duplicated_fund.save
+
+    assert DuplicateFundWarning.find_by(fund_id_1: @fund.id)
+  end
+
+  test "should create a duplicate fund warning due same fund manager and alias" do
+    duplicated_fund = Fund.new(fund_manager_id: @fund_manager.id, name: "Fund One", start_year: "2009")
+    duplicated_fund.save
+
+    assert DuplicateFundWarning.find_by(fund_id_2: duplicated_fund.id)
   end
 end
