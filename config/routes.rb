a2be66ff32a19a0d55342588543896fd3e9ca78d
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 13:09:55 -0600

    Adding missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  get "/duplicate_fund_warnings" => "duplicate_fund_warning#index"
+  delete "/duplicate_fund_warning/:id" => "duplicate_fund_warning#resolve", :as => :duplicate_fund_warning
+  
   resources :companies, except: [:show]
   resources :fund_managers, except: [:show]
   resources :funds, except: [:show]
