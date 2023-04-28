41c8e9ab70a135ea7638aa90df761dedd6227e5e
Luis Carlos <luis.carlos.finde@gmail.com>
2023-04-28 11:40:05 -0600

    Adding fund managers constraints and tests

--- app/models/fund_manager.rb
+++ app/models/fund_manager.rb
@@ -1,4 +1,7 @@
 class FundManager < ApplicationRecord
   has_many :funds
   belongs_to :company
+
+  validates :name , presence: true, allow_blank: false
+  validates :name, uniqueness: true
 end
--- test/models/fund_manager_test.rb
+++ test/models/fund_manager_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class FundManagerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @company = Company.create(name: "Test company")
+  end
+
+  test "should create a fund manager" do
+    fund_manager = FundManager.new(company_id: @company.id, name: @company.name)
+    
+    assert fund_manager.save
+    assert_equal fund_manager.name, "Test company"
+  end
+
+  test "should not create a fund manager without company" do
+    fund_manager = FundManager.new(company_id: nil)
+
+    assert_not fund_manager.save
+  end
 end
